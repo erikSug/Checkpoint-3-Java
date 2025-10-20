eba2b7bd634caa03346c4ffaba29afbce6c706ea
Erik Kaiyu Suguiyama <eriksug4@gmail.com>
2025-10-20 19:44:49 -0300

    feat: Adicionando os botões ao lado esquerdo da tela

--- src/main/java/com/example/todolist/ToDoList.java
+++ src/main/java/com/example/todolist/ToDoList.java
@@ -1,8 +1,16 @@
 package com.example.todolist;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,12 +18,49 @@ import java.io.IOException;
 public class ToDoList extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        Group root = new Group();
-        Scene scene = new Scene(root);
+        BorderPane root = new BorderPane();
+        Scene scene = new Scene(root, 1280, 720);
 
+        VBox leftMenu = new VBox(10);
+        leftMenu.setPadding(new Insets(10));
+
+        Label menuTitle = new Label("Tarefas");
+        Button btnNewTask = new Button("New Task");
+        Button btnToDo = new Button("To Do");
+        Button btnDoing = new Button("Doing");
+        Button btnDone = new Button("Done");
+        Button btnArchived = new Button("Archived");
+        Button btnCanceled = new Button("Canceled");
+
+        menuTitle.setPrefSize(150, 50);
+        menuTitle.setAlignment(Pos.CENTER);
+        menuTitle.setFont(Font.font("Verdana", FontWeight.BOLD, 20));
+
+        btnNewTask.setPrefSize(150, 50);
+        btnToDo.setPrefSize(150, 50);
+        btnDoing.setPrefSize(150, 50);
+        btnDone.setPrefSize(150, 50);
+        btnArchived.setPrefSize(150,50);
+        btnCanceled.setPrefSize(150,50);
+
+        leftMenu.getChildren().setAll(
+            menuTitle,
+            btnNewTask,
+            btnToDo,
+            btnDoing,
+            btnDone,
+            btnArchived,
+            btnCanceled
+        );
+
+
+        root.setLeft(leftMenu);
         stage.setScene(scene);
         stage.show();
 
 
+    }
+    public static void main(String[] args) {
+        launch();
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.todolist {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
 
     opens com.example.todolist to javafx.fxml;
